f10bb76cafe4f4e04a4403dad44ecd63de9a679c
smiksha1701 <sakevich2001@gmail.com>
2021-10-02 21:08:07 +0300

    minor decorative changes

--- Trees/Graph.py
+++ Trees/Graph.py
@@ -2,34 +2,34 @@ import matplotlib.pyplot as plt
 import networkx as nx
 class MyGraph(nx.Graph):
     def Diameter(self):
-        self.diamlen=nx.diameter(self.component)
+        self.diamlen = nx.diameter(self.component)
         for somenode in self.component.nodes:
             for anothernode in self.component.nodes:
                 if nx.shortest_path_length(self.component, somenode, anothernode) == self.diamlen:
-                    diametern=nx.shortest_path(self.component, source = somenode, target = anothernode)
+                    diametern = nx.shortest_path(self.component, source = somenode, target = anothernode)
         return diametern
-    def dfs(self,v):
+    def dfs(self, v):
         self.N.append(v)
         for w in self.neighbors(v):
-            if(self.pozn[w]==-1):
-                self.rebra_kist.append((w,v))
-                self.pozn[w]=0
+            if(self.pozn[w] == -1):
+                self.rebra_kist.append((w, v))
+                self.pozn[w] = 0
                 self.dfs(w)
     def Kist(self):
-        self.pozn=dict.fromkeys(self.component.nodes,-1)
-        self.v=next(iter(self.component.nodes))
-        self.pozn[self.v]=0
+        self.pozn = dict.fromkeys(self.component.nodes, -1)
+        self.v = next(iter(self.component.nodes))
+        self.pozn[self.v] = 0
         self.dfs(self.v)
     def Supgraph(self):
-        self.rebra_kist=[]
-        self.N=[]
+        self.rebra_kist = []
+        self.N = []
         self.diameter_nodes = []
         self.diameter_edges = []
         for numberofcomponent, c in enumerate(nx.connected_components(self)):
             self.component = self.subgraph(c)
             self.diam = self.Diameter()
             self.diameter_nodes.extend(self.diam)
-            self.numberofcomponent=numberofcomponent
+            self.numberofcomponent = numberofcomponent
             self.Subgraph_info()
             self.Output_Diameter()
             self.Kist()
@@ -37,7 +37,7 @@ class MyGraph(nx.Graph):
         print(" Diameter edges:")
         for i in range(len(self.diam)-1):
             self.diameter_edges.append((self.diam[i], self.diam[i+1]))
-            print(" {}-{}".format(self.diam[i],self.diam[i+1]),end=" ")
+            print(" {}-{}".format(self.diam[i], self.diam[i+1]), end = " ")
         print()
     def Nodes_Edges(self):
         print(" has:\n nodes:", len(self.component.nodes), "\n edges:", len(self.component.edges))
@@ -54,7 +54,7 @@ class MyGraph(nx.Graph):
     def Subgraph_Diameter(self):
         print("Diameter of component:", self.diamlen)
     def Subgraph_info(self):
-        print("{} component".format(self.numberofcomponent+1),end="")
+        print("{} component".format(self.numberofcomponent + 1), end = "")
         self.Nodes_Edges()
         self.Degrees()
         self.Eccentricitys()
@@ -69,19 +69,20 @@ color6 = 'y'
 G = nx.read_edgelist("data.txt", create_using = MyGraph(), nodetype = str)
 plt.figure(1)
 nx.draw(G, node_color = color1, edgecolors = color2, with_labels = True, font_color = color2)
-nodes_coords = {'A':(1,1),'B':(6,1),'C':(9,2),'D':(7,2),'E':(4,2),'F':(5,4),'G':(-4,4), 'J':(-5,1),'I':(-2,2), 'K':(11,4), 'L':(13,1),'M':(0,2),'N':(10,2),'O':(0,4)}
+nodes_coords = {'A' : (1, 1), 'B' : (6, 1), 'C' : (9, 2), 'D' : (7, 2), 'E' : (4, 2), 'F' : (5, 4), 'G' : (-4, 4), 
+'J' : (-5, 1), 'I' : (-2, 2), 'K' : (11, 4), 'L' : (13, 1), 'M' : (0, 2), 'N' : (10, 2), 'O' : (0, 4)}
 plt.figure(2)
 nx.draw(G, pos = nodes_coords, node_color = color1, edgecolors = color2, with_labels = True, font_color = color2)
-plt.savefig("Graph2.png", format="PNG")
+plt.savefig("Graph2.png", format = "PNG")
 G.Supgraph()
 plt.figure(3)
 nx.draw(G, pos = nodes_coords, node_color = color1, edgecolors = color2, with_labels = True, font_color = color2)
 nx.draw_networkx_nodes(G, nodelist = G.diameter_nodes, pos = nodes_coords, node_color = color3)
 nx.draw_networkx_edges(G, edgelist = G.diameter_edges, pos = nodes_coords, edge_color = color4, width = 5)
-plt.savefig("Graph3.png", format="PNG")
+plt.savefig("Graph3.png", format = "PNG")
 plt.figure(4)
 nx.draw(G, pos = nodes_coords, node_color = color1, edgecolors = color2, with_labels = True, font_color = color2)
 nx.draw_networkx_edges(G, edgelist = G.rebra_kist, pos = nodes_coords, edge_color = color6, width = 5)
-plt.savefig("Graph4.png", format="PNG")
+plt.savefig("Graph4.png", format = "PNG")
 plt.show(block = False)
 input()
